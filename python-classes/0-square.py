6945b121cdd8cffe64fce78e600f1187d1add5b7
Naellito <naelelafia12@gmail.com>
2025-05-21 10:19:21 +0200

    Adding the permision to all file

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Square that defines a square.
+"""
+
+
+class Square:
+    """
+    Empty class Square that defines a square.
+    """
+    pass
